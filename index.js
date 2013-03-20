8f32a953c15bf3e9ab94c89979b82a3135b27c50
Sunil Pai <threepointone@gmail.com>
2013-03-20 18:18:38 +0530

    offsets, signs, tweaks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function transform(pxlen, height, x, y, z) {
+function transform(pxlen, height, x, y, z, offset) {
     // convert a point from 
     // isometric x, y, z space to 
     // top, left, zIndex
-
+    offset = offset || {}
 
     pxlen = pxlen || 24;
     height = height || 33;
@@ -12,9 +12,9 @@ function transform(pxlen, height, x, y, z) {
     z = z || 0;
 
     return {
-        left: ((x - y) * pxlen) + 'px',
-        top: (((x + y) * 0.6 * pxlen) + (z * height)) + 'px',
-        zIndex: Math.round(1000 + (1000 * z) - ((x + y)))
+        left: ((offset.left || 0) + ((x - y) * pxlen)) + 'px',
+        top: ((offset.top || 0 ) - (((x + y) * 0.6 * pxlen) + (z * height))) + 'px',
+        zIndex: Math.round(1000 + (1000 * z) - ((x + y))) + ''
     }
 }
 
@@ -24,21 +24,24 @@ var faceMap = {
         skewX: '-30deg',
         skewY: '0deg',
         translateX: '0px',
-        translateY: '0px'
+        translateY: '0px',
+        scaleY:'1'
     },
     left: {
         rotateZ: '30deg',
         skewX: '30deg',
         skewY: '0deg',
         translateX: '-30px',
-        translateY: '14.2px'
+        translateY: '14.2px',
+        scaleY:'1'
     },
     top: {
         rotateZ: '-30deg',
         skewX: '30deg',
         skewY: '0deg',
-        translateX: '15px',
-        translateY: '-30px'
+        translateX: '16px',
+        translateY: '-28px',
+        scaleY:'0.88'
     }
 };
 
